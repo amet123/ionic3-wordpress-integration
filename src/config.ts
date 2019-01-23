9fd7963d3b321c57f43d75292212c9cc9f979557
amet123 <ametvaid@gmail.com>
2019-01-23 13:01:35 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 //config constants
-export const WORDPRESS_URL = 'http://wordpress.startapplabs.com/blog/';
+export const WORDPRESS_URL = 'http://1ne.in/S/';
 export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
